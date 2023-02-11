2ba288b131b590e7c738a37ce4450fadf052e7c8
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-02-11 12:23:13 +0300

    create

---
+++ App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import {} from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { AuthRoute, MainRoute } from "./router";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+  });
+};
+const MainStack = createStackNavigator();
+
+export default function App() {
+  const [auth, setAuth] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={loadFonts}
+        onFinish={() => setIsReady(true)}
+        onError={(error) => console.log(error)}
+      />
+    );
+  }
+  return (
+    <NavigationContainer>
+      <MainStack.Navigator>
+        <MainStack.Screen
+          options={{
+            headerShown: false,
+          }}
+          name="Auth"
+          component={AuthRoute}
+        />
+        <MainStack.Screen
+          options={{
+            headerShown: false,
+            headerRight: () => (
+              <Button
+                onPress={() => alert("This is a button!")}
+                title="Info"
+                color="#fff"
+              />
+            ),
+          }}
+          name="Posts"
+          component={MainRoute}
+        />
+      </MainStack.Navigator>
+    </NavigationContainer>
+  );
+}
